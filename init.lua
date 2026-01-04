1071327edf6f98cd482988dcd0f55e12ccb71e51
Rizaldi Aziz <rizaldi.aziz@bluebirdgroup.com>
2026-01-04 11:51:53 +0700

    adjust lil bit

--- init.lua
+++ init.lua
@@ -920,8 +920,9 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    -- main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
@@ -953,7 +954,7 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
--- lua/custom/plugins/riaz.lua
+++ lua/custom/plugins/riaz.lua
@@ -21,9 +21,11 @@ vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv")
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'cs', -- This applies to C# files
   callback = function()
-    vim.opt_local.tabstop = 2 -- Tabs are 4 spaces
-    vim.opt_local.shiftwidth = 2 -- Indent by 4 spaces
+    vim.opt_local.tabstop = 4 -- Tabs are 4 spaces
+    vim.opt_local.shiftwidth = 4 -- Indent by 4 spaces
     vim.opt_local.smartindent = true -- Enable smart indentation
+    vim.opt_local.expandtab = true
+    vim.opt_local.softtabstop = 4
     vim.bo.commentstring = '//%s'
   end,
 })
